56918c4f6ea2189ff6643da3a8e04eb1f624e7ce
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2023-10-26 23:52:23 +0500

    changes made

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 // Swiper
 var swiper = new Swiper(".mySwiper", {
-    speed: 600,
-    parallax: true,
-    autoplay: {
-      delay: 4000, 
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 4000,
     },
   });
 
